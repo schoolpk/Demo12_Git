307fdcd1b4772202c4ef8e8798a51b72af11eacc
Pham Ke Truong <truongpk2k5@gmail.com>
2024-02-23 19:38:18 +0700

    Print Hello2

--- src/com/tn/Program.java
+++ src/com/tn/Program.java
@@ -3,5 +3,7 @@ package com.tn;
 public class Program {
     public static void main(String[] args) {
         System.out.println("Hello Git");
+        System.out.println("Hello Git 2");
+        System.out.println("Hello Git 3");
     }
 }
